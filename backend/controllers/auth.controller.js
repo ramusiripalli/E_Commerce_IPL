e46fad86c5c74a3ecfdc0f06d00f00dfe1307d4b
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-30 19:32:09 +0530

    auth controller Setup
    auth controller Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req,res)=>{
+    res.send("Sign up Route called")
+}
+
+export const login = async (req,res)=>{
+    res.send("login Route called")
+}
+
+
+export const logout = async (req,res)=>{
+    res.send("logout Route called")
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from 'express';
+import { login, logout, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get("/signup",(req,res)=>{
-    res.send("Sign up Route called")
-});
+router.get("/signup",signup);
 
-router.get("/login",(req,res)=>{
-    res.send("Login Route called")
-});
+router.get("/login",login);
 
-router.get("/logout",(req,res)=>{
-    res.send("Logout Route called")
-});
+router.get("/logout",logout);
 
 export default router;
