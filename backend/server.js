4b75291e697a1d7304a873b1448180818862c84d
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-30 20:03:50 +0530

    Connecting to Mongo Db
    Mongo Db Connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import {connectDB} from './lib/db.js'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,4 +10,6 @@ app.use('/api/auth',authRoutes);
 
 app.listen(PORT,()=>{
     console.log("server is running on http://localhost:" + PORT);
+    connectDB();
 })
+
