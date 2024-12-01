65c5458f77be3c02bf7c255672826b4576f546db
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-01 09:36:04 +0530

    Sign up controller,  adding user to database
    Sign up controller,  adding user to database

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import {connectDB} from './lib/db.js'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(express.json());
 app.use('/api/auth',authRoutes);
 
 app.listen(PORT,()=>{
