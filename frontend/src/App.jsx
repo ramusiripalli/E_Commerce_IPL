f413e324f1b6705ffb84a06fc72ba86d91f60a9e
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-02 12:23:16 +0530

    Toaster and added target url to reduce cors error
    Toaster and added target url to reduce cors error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import Navbar from './components/Navbar.jsx';
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Route path='/login' element={<LoginPage />} />
     </Routes>
     </div>
+    <Toaster />
 </div>
   )
 }
