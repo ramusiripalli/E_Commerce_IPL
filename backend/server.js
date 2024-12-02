2cf93019823f2dc936c14cbc7085235b13f8c249
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-02 06:53:10 +0530

    added jwt and store in cookies and reddis , tested signup,login,logout, refreshToken and get Profile
    added jwt and store in cookies and reddis , tested signup,login,logout, refreshToken and get Profile

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 import {connectDB} from './lib/db.js'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 
 app.listen(PORT,()=>{
