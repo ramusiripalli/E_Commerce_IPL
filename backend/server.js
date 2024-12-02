eb452aa0bbdba84b5274f8e8fb3bbd2b6e1301d8
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-02 10:23:54 +0530

    Product Model,controller and middleware for admin and protect route. and cloudinary details
    Product Model,controller and middleware for admin and protect route. and cloudinary details

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
 import cookieParser from "cookie-parser";
 import {connectDB} from './lib/db.js'
 dotenv.config();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth',authRoutes);
+app.use('/api/products',productRoutes);
 
 app.listen(PORT,()=>{
     console.log("server is running on http://localhost:" + PORT);
