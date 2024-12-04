22e2d19d35ee295e123ee83bef1b2e2230c36acc
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-04 16:49:39 +0530

    updated useUseStore zustand
    updated useUseStore zustand

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import {connectDB} from './lib/db.js'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/products',productRoutes);
