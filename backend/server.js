780ec7ccf4faa6f5dd45a018ff7d05102c8c0a6f
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-04 23:42:21 +0530

    Made some changes to Server.js for working on deployement
    Made some changes to Server.js for working on deployement

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,24 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cookieParser from "cookie-parser";
-import {connectDB} from './lib/db.js'
+import {connectDB} from './lib/db.js';
+import path from 'path';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/products',productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log("server is running on http://localhost:" + PORT);
